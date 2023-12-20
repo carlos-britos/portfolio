a66067afb7932ae1afa2a6b40df89ead8ac6e91a
Carlos <carlos.britos@web-experto.com>
2023-12-20 03:52:10 -0300

    Add first section

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,4 +1,5 @@
 import heroImage from '../../assets/images/hero-image.jpg'
+import inergioImage from '../../assets/images/inergio.png'
 // import { LandingSection } from '../components/LandingSection';
 // import { CardSection } from '../components/CardSection';
 // import { UsNetworks } from '../components/UsNetworks';
@@ -10,7 +11,12 @@ function Home() {
         <div className="hero__wrapper">
           <div className="hero__info">
             <h1 className="info__title">
-              Carlos Britos - Semi Sr UI Developer
+              <div className="name">
+                Carlos Britos
+              </div>
+              <div className="range">
+                Semi Sr <span className="ui">UI</span> Developer
+              </div>
             </h1>
 
             <h3 className="info__description">
@@ -30,6 +36,32 @@ function Home() {
           <div className="hero__img">
             {/* TODO: Optimizar imagen */}
             <img src={heroImage} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className='proyects freelance'>
+        <h1 className='proyects__title'>Proyectos freelance</h1>
+
+        {/* TODO: agregar swiper */}
+        <div className="cards">
+          <div className="wrapper">
+            <div className="card">
+              {/* TODO: Optimizar y mejorar la imagen */}
+              <img src={inergioImage} alt="Pantallazo principal de la web inergio.com.ar" />
+              <div className="info">
+                <div className="info__title">Inergio</div>
+
+                <div className="info__description">
+                  Landing web creada para conocer sus servicios y contactar de forma rapida y sencilla
+                </div>
+
+                <div className="info__tecnologies">
+                  {/* TODO: Pasar a chips */}
+                  React Sass / CSS Vite
+                </div>
+              </div>
+              
+            </div>
           </div>
         </div>
       </section>
