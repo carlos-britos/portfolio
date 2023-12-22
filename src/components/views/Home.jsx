4738eff234b7a4e7145607a20d1a48491ab092e4
Carlos <carlos.britos@web-experto.com>
2023-12-22 02:50:34 -0300

    Add images with flip

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,10 +1,45 @@
 import heroImage from '../../assets/images/hero-image.jpg'
 import inergioImage from '../../assets/images/inergio.png'
+import inergioDesktopImage from '../../assets/images/inergio-desktop.png'
+import { useState, useRef, forwardRef, useImperativeHandle } from 'react'
 // import { LandingSection } from '../components/LandingSection';
 // import { CardSection } from '../components/CardSection';
 // import { UsNetworks } from '../components/UsNetworks';
 
+function BothImages({ changeImage }, ref) {
+  
+  const [isDesktop, setIsDesktop] = useState(false)
+
+  useImperativeHandle(ref, () => ({
+    changeImage: () => {
+      setIsDesktop(!isDesktop);
+    },
+  }));
+  
+  return(
+    <div className={`images ${isDesktop ? 'show' : ''}`}>
+      {/* TODO: Optimizar y mejorar la imagen */}
+      {/* TODO: Hacer imagenes con alto y ancho dinamico con js */}
+      <img className={`img-mobile ${isDesktop ? '' : 'show'}`} src={inergioImage} alt="Pantallazo principal de la web inergio.com.ar" decoding='async' loading='lazy'/>
+      <img className={`img-desktop ${isDesktop ? 'show' : ''}`} src={inergioDesktopImage} alt="Pantallazo principal de la web inergio.com.ar" decoding='async' loading='lazy'/>
+    </div>
+  )
+}
+
+const ForwardedBothImages = forwardRef(BothImages);
+
 function Home() {
+  // TODO: Eliminar cuando se agreguen los proyectos
+  const loop = [1,2,3,4,5,6,7,8]
+
+  const imageRefs = loop.map(() => useRef(null));
+
+  const callChangeImage = (index) => {
+    if (imageRefs[index].current) {
+      imageRefs[index].current.changeImage();
+    }
+  };
+
   return (
     <main>
       <section className="hero">
@@ -39,29 +74,39 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className='proyects freelance'>
-        <h1 className='proyects__title'>Proyectos freelance</h1>
+      <section className='projects freelance'>
+        <h1 className='projects__title'>Proyectos freelance</h1>
 
         {/* TODO: agregar swiper */}
         <div className="cards">
           <div className="wrapper">
-            <div className="card">
-              {/* TODO: Optimizar y mejorar la imagen */}
-              <img src={inergioImage} alt="Pantallazo principal de la web inergio.com.ar" decoding='async' loading='lazy'/>
-              <div className="info">
-                <div className="info__title">Inergio</div>
-
-                <div className="info__description">
-                  Landing web creada para conocer sus servicios y contactar de forma rapida y sencilla
-                </div>
-
-                <div className="info__tecnologies">
-                  {/* TODO: Pasar a chips */}
-                  React Sass / CSS Vite
+            {loop.map((item, index) => (
+            
+              <div key={item} className="card">
+                <ForwardedBothImages ref={imageRefs[index]} />
+                
+                <div className="info__wrapper">
+                  <div className="info">
+                    <div className="info__buttons">
+                      <div className="change-view" onClick={() => callChangeImage(index)}>
+                        Ver desktop
+                      </div>
+                      <div className="github">
+                        Github
+                      </div>
+                    </div>
+                    <div className="info__title">Inergio</div>
+                    <div className="info__description">
+                      Landing web creada para conocer sus servicios y contactar de forma rapida y sencilla
+                    </div>
+                    <div className="info__tecnologies">
+                      {/* TODO: Pasar a chips */}
+                      React Sass / CSS Vite
+                    </div>
+                  </div>
                 </div>
               </div>
-              
-            </div>
+            ))}
           </div>
         </div>
       </section>
