c48f1572d8f466b898c68f1df6df56fc04f8cee2
Carlos <carlos.britos@web-experto.com>
2023-12-30 03:50:43 -0300

    Fix project cards

--- src/components/shared/ProjectPreview.jsx
+++ src/components/shared/ProjectPreview.jsx
@@ -1,49 +1,65 @@
-import inergioImage from '../../assets/images/inergio.png'
-import inergioDesktopImage from '../../assets/images/inergio-desktop.png'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Icon } from '../icons/Icon'
-import { Desktop } from '../icons/Desktop'
-import { Mobile } from '../icons/Mobile'
 import { Github } from '../icons/Github'
+import { Mobile } from '../icons/Mobile'
+import { Desktop } from '../icons/Desktop'
 
-function ProjectPreview() {
+
+function ProjectPreview({ imgMobile, imgDesktop, alt, title, description, tecnologies }) {
   const [showPreviewDesktop, setShowPreviewDesktop] = useState(false)
-  // const previewRef = useRef(null)
+  const [showButtonChanged, setShowButtonChanged] = useState(false)
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setShowButtonChanged(window.innerWidth < 992)
+    };
+
+    handleWindowResize()
+    
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [])
 
   return (
     <div className="card">
       <div className={`images ${showPreviewDesktop ? 'is-desktop' : ''}`}>
         {/* TODO: Optimizar y mejorar la imagen */}
-        <img className="img-mobile" src={inergioImage} alt="Pantallazo principal de la web inergio.com.ar" decoding='async' loading='lazy'/>
-        <img className="img-desktop" src={inergioDesktopImage} alt="Pantallazo principal de la web inergio.com.ar" decoding='async' loading='lazy'/>
+        <img className="img-mobile" src={imgMobile} alt={alt} decoding='async' loading='lazy'/>
+        <img className="img-desktop" src={imgDesktop} alt={alt} decoding='async' loading='lazy'/>
       </div>
-              
+
       <div className="info__wrapper">
         <div className="info">
-          <div className="info__buttons">
-            <div className="change-view c-button c-button--outlined c-button--with-icon" onClick={() => setShowPreviewDesktop(!showPreviewDesktop)}>
-              { showPreviewDesktop ? <Icon iconSvg={<Mobile />} /> : <Icon iconSvg={<Desktop />} /> }
-              { showPreviewDesktop ? 'Ver mobile' : 'Ver desktop'}
+          <div className="info__top">
+            <div className="info__buttons">
+
+              {showButtonChanged && (
+                <div className="change-view c-button c-button--outlined c-button--with-icon" onClick={() => setShowPreviewDesktop(!showPreviewDesktop)}>
+                  { showPreviewDesktop ? <Icon iconSvg={<Mobile />} /> : <Icon iconSvg={<Desktop />} /> }
+                  { showPreviewDesktop ? 'Ver mobile' : 'Ver desktop'}
+                </div>
+              )}
+
+              <div className="github c-button c-button--outlined c-button--only-icon">
+                <Icon iconSvg={<Github />} />
+              </div>
             </div>
-            <div className="github c-button c-button--outlined c-button--only-icon">
-              <Icon iconSvg={<Github />} />
+            <div className="info__title">
+              {title}
             </div>
           </div>
-          <div className="info__title">Inergio</div>
           <div className="info__description">
-            Landing web creada para conocer sus servicios y contactar de forma rapida y sencilla
+            {description}
           </div>
           <div className="info__tecnologies">
-            {/* TODO: Pasar a chips */}
-            <div className="chip">
-              React
-            </div>
-            <div className="chip">
-              Sass / CSS
-            </div>
-            <div className="chip">
-              Vite
-            </div>
+            {tecnologies.map((chip, index) => (
+              <div key={index} className="chip">
+                {chip}
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/shared/SectionProjects.jsx
+++ src/components/shared/SectionProjects.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import { ProjectPreview } from "./ProjectPreview"
+import inergioImage from '../../assets/images/inergio.png'
+import inergioDesktopImage from '../../assets/images/inergio-desktop.png'
 
 function SectionProjects({ title, classes }) {
   // TODO: Eliminar cuando se agreguen los proyectos
@@ -12,7 +14,15 @@ function SectionProjects({ title, classes }) {
       <div className="cards">
         <div className="wrapper">
           {loop.map((item, index) => (
-            <ProjectPreview key={index} />
+
+            <ProjectPreview key={index} 
+              imgMobile={inergioImage} 
+              imgDesktop={inergioDesktopImage} 
+              alt={'Pantallazo principal de la web inergio.com.ar'}
+              title='Inergio'
+              description='Landing web creada para conocer sus servicios y contactar de forma rapida y sencilla'
+              tecnologies={['React', 'Sass / CSS', 'Vite']} >
+            </ProjectPreview>
           ))}
         </div>
       </div>
